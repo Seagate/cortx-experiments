3d14c2531d56d43d275cb1061dd43757879f1dc4
Rajkumar <49739659+rajkumarpatel2602@users.noreply.github.com>
2020-07-07 16:21:24 +0530

    Update consulCluster.py

--- consul/src/consulCluster.py
+++ consul/src/consulCluster.py
@@ -32,7 +32,7 @@ def ConsulMembers():
     print ("member list  : ")
     mem = c.agent.members(wan=False)
     for var in mem:
-        print(var)
+        print(var,'\n')
 
 
 def KvstorePut():
@@ -52,18 +52,84 @@ def KvGetAll():
 
 def KvDeletekey():
     key = raw_input('Input key to delete')
-    cmd = 'cosnul kv delte ',key
+    c.kv.delete(key)
 
 def LeaderInfo():
     print(c.status.leader())
     #print(c.status.peers())
 
-def RestartAgent():
-    print ("leaving consul")
-    os.system('consul leave')
+def startSession():
+    print ("starting the session : \n",c.session.create())
 
 def LeaveConsul():
     os.system('consul leave')
+
+def ListSessions():
+    t = c.session.list()
+    for i in t[1:]:
+        for x in i:
+            print (x,'\n')
+
+def KvPutWithLockAcquire():
+    session_id = raw_input('provide session_id to acquire lock :\n')
+    key = raw_input('provide key :\n')
+    val = raw_input('provide value :\n')
+
+    if True == c.kv.put(key, val, acquire=session_id):
+        print('Acquisition succeed!\n')
+    else:
+        print('Acquisition Failed!\n')
+
+def KvPutReleaseLock():
+    session_id = raw_input('provide session_id to acquire lock :\n')
+    key = raw_input('provide key :\n')
+    if True == c.kv.put(key, None, release=session_id):
+        print('Release succeed!\n')
+    else:
+        print('Release Failed!\n')
+
+def DestroySession():
+    session_id = raw_input('Input session_id to destroy!')
+    if True == c.session.destroy(session_id):
+        print('Destroy succeed!\n')
+    else:
+        print('Destroy Failed!\n')
+
+
+def CreateACL():
+    tInput = int(raw_input('enter 0 for "management" token\nenter 1 for "client" token\n'))
+    rule = None
+    if tInput == 0:
+        tokenType = 'management'
+    else:
+        tokenType = 'client'
+    
+    tokenName = raw_input('Name of the token[Optional]\n')
+    rule = raw_input('Enter rule for this token [Note: Json or Hcl format is must\n]')
+    bootStrapToken = raw_input("enter bootstrap/management token to perfomr token update\n")
+    
+    #management token passing is must[bootstrap token is something one can use]
+    token_id = c.acl.create(name=tokenName, type=tokenType, rules=rule, acl_id=None, token=bootStrapToken)
+
+    print('acl_id is : ', token_id)
+
+
+
+def UpdateACL():
+    tInput = int(raw_input('enter 0 for "management" token\nenter 1 for "client" token\n'))
+    rule = None
+    if tInput == 0:
+        tokenType = 'management'
+    else:
+        tokenType = 'client'
+    
+    acl_id = raw_input("enter token to update policy :\n")
+    bootStrapToken = raw_input("enter bootstrap/management token to perform token update\n")
+    rule = raw_input('Enter rule for this token [Note: Json or Hcl format is must\n]')
+    token_id = c.acl.update(acl_id, name=None, type=tokenType, rules=rule, token=bootStrapToken)
+    
+    print('ACL update returns : ', token_id)
+    
 
 def Switch(argument):
     switcher = {
@@ -75,9 +141,16 @@ def Switch(argument):
         6: KvGetAll,
         7: KvDeletekey,
         8: LeaderInfo,
-        9: RestartAgent,
+        9: startSession,
         0: LeaveConsul,
+        11: ListSessions,
+        12: KvPutWithLockAcquire,
+        13: KvPutReleaseLock,
+        14: DestroySession,
+	    15: CreateACL,
+	    16: UpdateACL,
     }
+    #function call
     switcher[argument]()
 
 
@@ -91,8 +164,14 @@ def printList():
     print("press 6 to get all key-val")
     print("press 7 to delete a key-val pari")
     print("press 8 for leader info")
-    print("press 9 to restart an agent")
-    print("press 0 to leave consul\n")
+    print("press 9 to create a session")
+    print("press 0 to leave consul")
+    print("press 11 to list sessions")
+    print("press 12 to acquire lock")
+    print("press 13 to release lock")
+    print("press 14 to destroy session")
+    print("press 15 to create ACL")
+    print("press 16 to update ACL")
     print("******************************************\n")
 
     choice = int(input())
@@ -106,7 +185,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
