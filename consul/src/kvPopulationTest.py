5ca761752d6db1ef295daf08e3bb058613b3b999
Rajkumar <49739659+rajkumarpatel2602@users.noreply.github.com>
2020-07-08 09:38:05 +0530

    Create kvPopulationTest.py

---
+++ consul/src/kvPopulationTest.py
@@ -0,0 +1,41 @@
+# This test is used to create 0.1 Million objects of 1KB size.
+# 1KB content is taken from a preexisting file named [my1kFile.txt] in this case.
+# one can use anyfile instead, this modification is required.
+
+from __future__ import print_function
+import consul
+import os
+import subprocess
+import time
+
+#c = consul.Consul(host='10.2302.241.123', )
+#c = consul.Consul(host='10.230.241.123', port='1301')
+#c = consul.Consul()
+
+start = 0
+end = 0
+
+def createObjs():
+    start = time.time()
+    for i in range(100000):
+        if i%1000 == 0:
+            end = time.time()
+            command = 'echo '+'Runtime after '+str((i+1))+' is :'+str((end-start))+'>> logfileInterval.txt' 
+            os.system(command)
+            #os.system("echo 'runtime of the program is : ' ''"print("Runtime of the program is : ",(end - start))
+
+        filename = 'keyFile_'+str(i)+'.txt'
+        
+        #c.kv.put(filename, '@myFile.txt')
+        os.system('consul kv put '+filename+' @my1kFile.txt')
+
+    end = time.time()
+    print("Runtime of the program is : ",(end - start))
+   
+
+def main():
+	createObjs()
+
+if __name__ == '__main__':
+    main()
+
