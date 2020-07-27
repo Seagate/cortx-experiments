15ab242438bf57f8e8e5203ce17d8b019f24eded
Rajkumar <49739659+rajkumarpatel2602@users.noreply.github.com>
2020-07-27 16:59:59 +0530

    Update consul_acl.py

--- consul/src/consul_acl.py
+++ consul/src/consul_acl.py
@@ -5,38 +5,7 @@ from __future__ import print_function
 import consul
 import os
 import subprocess
-#import socket
-  
-# To perform KV 'put' with a session locking
-def KvPutWithLockAcquire():
-    session_id = raw_input('provide session_id to acquire lock :\n')
-    key = raw_input('provide key :\n')
-    val = raw_input('provide value :\n')
-
-    if True == c.kv.put(key, val, acquire=session_id):
-        print('Acquisition succeed!\n')
-    else:
-        print('Acquisition Failed!\n')
-
-	
-# To release session lock on a particular key 
-def KvPutReleaseLock():
-    session_id = raw_input('provide session_id to acquire lock :\n')
-    key = raw_input('provide key :\n')
-    if True == c.kv.put(key, None, release=session_id):
-        print('Release succeed!\n')
-    else:
-        print('Release Failed!\n')
-
-	
-# To destroy created session
-def DestroySession():
-    session_id = raw_input('Input session_id to destroy!')
-    if True == c.session.destroy(session_id):
-        print('Destroy succeed!\n')
-    else:
-        print('Destroy Failed!\n')
-
+import socket
 	
 # To create ACL with your desired rules
 # Client options is to be selected for for simple policy creation and application
