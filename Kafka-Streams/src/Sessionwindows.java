1359ad37658fbeaa9826320f4dd1dcc8e0126891
Suprit Shinde <68838279+supritshinde@users.noreply.github.com>
2021-03-24 12:50:46 +0530

    Update Sessionwindows.java

--- Kafka-Streams/src/Sessionwindows.java
+++ Kafka-Streams/src/Sessionwindows.java
@@ -1,3 +1,7 @@
+/*This is an application to demonstrate the use of session windowing in kafka streams.
+This application does a simple word count operation. 
+Main motive was to understand Kafka streams DSL, Kafka and try out different windowing approaches for same.*/
+
 package org.apache.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -17,14 +21,14 @@ public class SessionWindow {
     public static void main(String[] args) {
 
         Properties streamsConfiguration = new Properties();
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "PUBG");
-        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "ssc-vm-c-0333.colo.seagate.com:9092, ssc-vm-c-0332.colo.seagate.com:9092, ssc-vm-0789.colo.seagate.com:9092");
-        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "Any name of your choice but should follow the app id variable name rules, eg: SEAGATEAPP");
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "your virtual machines fqdn:9092, eg: ssc-vm-1234.colo.seagate.com:9092");
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "(earliest/latest) depending on your requirement");
         streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, String> initialstream = builder.stream("TextLinesTopic", Consumed.with(Serdes.String(), Serdes.String()));
+        KStream<String, String> initialstream = builder.stream("NAME OF YOUR INPUT TOPIC", Consumed.with(Serdes.String(), Serdes.String()));
 
         KStream<String, String> Sstream = initialstream
                 .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")));
@@ -38,7 +42,7 @@ public class SessionWindow {
         Stable
                 .toStream()
                 .selectKey((key, word) -> key.key())
-                .to("sessionoutputtopic", Produced.with(Serdes.String(), Serdes.Long()));
+                .to("NAME OF THE OUTPUT TOPIC", Produced.with(Serdes.String(), Serdes.Long()));
 
         KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
         streams.start();
