dd457d5a70d0e94288fe36652b5e13d24b197c02
root <root@ssc-vm-g3-rhev4-2754.colo.seagate.com>
2021-12-13 04:13:15 -0700

    minor changes
    Signed-off-by: root <root@ssc-vm-g3-rhev4-2754.colo.seagate.com>

--- REST-client-in-cpp/TestClient.cpp
+++ REST-client-in-cpp/TestClient.cpp
@@ -1,3 +1,17 @@
+// Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+// For any questions about this software or licensing,
+// please email opensource@seagate.com or cortx-questions@seagate.com.
+
 #include <iostream>
 #include <vector>
 #include <map>
@@ -25,6 +39,7 @@ void displayResult(std::map<bool, int>& result)
     std::cout << "Number of failed Requests: " << result[false] << std::endl;
 }
 
+// You can test the class my running this main()
 int main(int argc, char* argv[])
 {
     if ((argc > 4) || (argc < 3))
